3c660709800388d6ce8871dc61d82af2def5692a
Vinicius Albino <69263845+Shinguek0@users.noreply.github.com>
2022-09-02 19:36:06 -0300

    OOP

---
+++ OOP/Lista/exec1/Conta.py
@@ -0,0 +1,4 @@
+#Escreva um método para a classe ContaCorrente() vista em aula, que permita a
+#transferência de valores entre duas contas (objetos). Esse método deverá receber
+#como parâmetro: valor, origem e destino.
+#Dica: o método deve sacar o valor da origem e depositar o valor no destino.
